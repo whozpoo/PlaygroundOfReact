3fc32eb37ef12f598b11c59add2d699f72051e2f
poo <pbrenna12@gmail.com>
2022-10-24 23:04:25 +0800

    Practiced using Refs to tile a picture wall.

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,10 +1,13 @@
+import "./ImageList.css";
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
-  const images = props.images.map(({ description, id, urls }) => (
-    <img alt={description} key={id} src={urls.regular} />
-  ));
-  return <div>{images}</div>;
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
+  });
+  console.log(images);
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
